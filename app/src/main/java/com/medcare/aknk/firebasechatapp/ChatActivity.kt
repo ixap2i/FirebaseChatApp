dabdb0ecfcce3a76c9b0577fdab9e50c0d37e344
aknk <aknk@aknk.local>
2019-01-26 14:39:48 +0900

    authentication setup complete

--- app/src/main/java/com/medcare/aknk/firebasechatapp/ChatActivity.kt
+++ app/src/main/java/com/medcare/aknk/firebasechatapp/ChatActivity.kt
@@ -1,23 +1,6 @@
 package com.medcare.aknk.firebasechatapp
 
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import com.google.firebase.iid.FirebaseInstanceId
-
 
 class ChatActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
-
-        val service = MyFirebaseInstanceIdService()
-        Log.d("check token", "Refreshed token: " + service.onNewToken())
-
-
-//        val service: FirebaseInstanceId = FirebaseInstanceId()
-
-//        service()
-    }
 }
