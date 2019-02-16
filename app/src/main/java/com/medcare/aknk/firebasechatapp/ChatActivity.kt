ec711a45e5c6a03422b71a7363870ff41ebd9ed3
ixap2i <ayamashita124@gmail.com>
2019-02-16 16:03:07 +0900

    add post chat message function

--- app/src/main/java/com/medcare/aknk/firebasechatapp/ChatActivity.kt
+++ app/src/main/java/com/medcare/aknk/firebasechatapp/ChatActivity.kt
@@ -3,27 +3,49 @@ package com.medcare.aknk.firebasechatapp
 import android.os.Bundle
 import android.app.Activity
 import android.util.Log
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import androidx.constraintlayout.widget.Constraints.TAG
+import androidx.room.Database
+import com.google.firebase.database.*
+import com.medcare.aknk.firebasechatapp.model.ChatMessage
+import com.medcare.aknk.firebasechatapp.model.ChatMessageManager
 
 import kotlinx.android.synthetic.main.activity_chat_base.*
+import org.json.JSONObject
 
 class ChatActivity : Activity() {
 
+    private lateinit var uid: String
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var message: String
     private lateinit var db: DatabaseReference
-//    private lateinit var databaseAdapter : ChatViewAdapter
+
+    private val  chatMessageMng = ChatMessageManager()
+
+    fun sendChatMessage(db: DatabaseReference, userId: String, email: String, message: String, imageUrl: String) {
+
+        db.addValueEventListener(chatMessageMng.postListener)
+
+
+        db.child("chat_message").setValue(userId)
+        db.child("chat_message").child(userId).child("user_name").setValue(email)
+        db.child("chat_message").child(userId).child("message").setValue(message)
+        db.child("chat_message").child(userId).child("image_url").setValue(imageUrl)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_base)
 
-        val email = intent.getStringExtra("email")
-        val password = intent.getStringExtra("password")
+        uid = intent.getStringExtra("uid")
+        email = intent.getStringExtra("email")
+        password = intent.getStringExtra("password")
+        message = "Test"
+        val imageUrl = ""
 
-        db = FirebaseDatabase.getInstance().getReference()
-        db.setValue("message").addOnCompleteListener {
-            Log.d("", "access was succeed!")
-        }
+        db = FirebaseDatabase.getInstance().getReference("message")
+
+        sendChatMessage(db, uid, email, message, imageUrl)
 
         Log.d("", "${db}")
         Log.d("", "Chat Activity started successful!")
--- app/src/main/java/com/medcare/aknk/firebasechatapp/model/ChatMessage.kt
+++ app/src/main/java/com/medcare/aknk/firebasechatapp/model/ChatMessage.kt
@@ -1,15 +1,44 @@
 package com.medcare.aknk.firebasechatapp.model
 
+import android.util.Log
+import androidx.constraintlayout.widget.Constraints
 import androidx.room.Embedded
 import androidx.room.Relation
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
-class ChatMessage {
+data class ChatMessage(
+    var chatMessage: String,
+    val email: String,
+    val ImageUrl: String?
+) {
 
     @Embedded
     lateinit var message: Message
 
     @Relation(parentColumn = "id", entityColumn = "message_id")
-    var messages: List<ChatMessage> = arrayListOf()
+    var messages: List<ChatMessage> = listOf()
+
+}
+
+class ChatMessageManager {
+
+    val postListener = object : ValueEventListener {
+        override fun onDataChange(dbSnapShot: DataSnapshot?) {
+//            val postData = dbSnapShot?.getValue(ChatMessage::class.java)
+                Log.d("", "write was success")
+
+    //            postData.let {
+//                Log.d("", "write was success")
+//                // TODO update ui
+//            }
+        }
+
+        override fun onCancelled(dbErr: DatabaseError?) {
+            Log.w(Constraints.TAG, "load post canceled", dbErr?.toException())
+        }
+    }
 }
 
 class Message {
